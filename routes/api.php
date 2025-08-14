e3c951951d42f74bd39119d0a00a1da0e38666a8
marien <mmanima322@gmail.com>
2025-08-13 18:32:49 -0700

    feat integration et test api

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/user', function (Request $request) {
+    return "bonjour";
+});
+
+// ->middleware('auth:sanctum');
+// Route
