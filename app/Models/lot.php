d2b8bfe10d337b43360a14b580aa50e77cb7379f
benikib <benikasu7@gmail.com>
2025-08-17 21:18:35 +0200

    feat add lots in pharmacie

--- app/Models/lot.php
+++ app/Models/lot.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class lot extends Model
 {
-    //
+    protected $primaryKey = 'id_lot';
+    protected $table = 'lots';
+    public $incrementing = true;
+    protected $keyType = 'int';
+
+    protected $fillable = [
+        "date_expiration",
+        "quantite",
+        "prix_achat",
+        "prix_unitaire",
+        "numero_lot",
+        "id_pharmacie",
+        "id_medicament"
+    ];
+
+    public function medicament()
+    {
+        return $this->belongsTo(Medicament::class, 'id_medicament', 'id_medicament');
+    }
 }
