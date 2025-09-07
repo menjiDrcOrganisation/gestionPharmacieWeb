ffe1f82b0873c707a8af37eeafdbaccfa07dec18
marien <mmanima322@gmail.com>
2025-09-07 21:01:02 +0100

    add GerantController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Mail\UserCreatedMail;
-use App\Models\Gerant;
+use App\Models\gerant;
 use App\Models\Pharmacie;
 use App\Models\User;
 use App\Models\Vendeur;
